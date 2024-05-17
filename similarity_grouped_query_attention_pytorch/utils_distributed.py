d79e7fcd107c49274a3470740318947702d19348
Athe-kunal <athekunal@gmail.com>
2024-05-17 00:01:03 -0400

    no savinf directories

--- similarity_grouped_query_attention_pytorch/utils_distributed.py
+++ similarity_grouped_query_attention_pytorch/utils_distributed.py
@@ -158,10 +158,10 @@ def train(
     weight_flag: bool = False,
     if_random: bool = False,
 ):
-    dir = logging_name.upper()
+    # dir = logging_name.upper()
     # if os.path.exists(dir):
     #     shutil.rmtree(dir)
-    os.makedirs(dir,exist_ok=True)
+    # os.makedirs(dir,exist_ok=True)
     device = torch.device("cuda", rank)
     device_id = rank % world_size
     t5: T5ForConditionalGeneration = T5ForConditionalGeneration.from_pretrained(
